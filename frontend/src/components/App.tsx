f1932e9ca5601644f1074da85f18a57d0299ba01
Nicholas Chumney <nicholas.chumney@outlook.com>
2026-04-26 11:47:20 -0400

    add a nav bar

--- frontend/src/components/App.tsx
+++ frontend/src/components/App.tsx
@@ -1,11 +1,13 @@
 import { Outlet, ScrollRestoration } from 'react-router-dom';
 
+import Navbar from './Navbar';
 import AuthProvider from '../providers/AuthProvider';
 
 const App = () => {
   return (
     <AuthProvider>
       <ScrollRestoration />
+      <Navbar />
       <main>
         <Outlet />
       </main>
