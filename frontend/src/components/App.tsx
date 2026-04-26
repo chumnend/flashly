1f067d758ffe8900e5811f21b8e3d6fc1f74ad94
Nicholas Chumney <nicholas.chumney@outlook.com>
2026-04-25 23:50:27 -0400

    reset React frontend

---
+++ frontend/src/components/App.tsx
@@ -0,0 +1,9 @@
+const App = () => {
+  return (
+    <div className='app'>
+      Flashly
+    </div>
+  )
+}
+
+export default App
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,18 +1,12 @@
 import { StrictMode } from 'react';
-import { BrowserRouter } from 'react-router-dom';
 import { createRoot } from 'react-dom/client';
 
-import App from './components/app/App/';
-import AuthProvider from './components/providers/AuthProvider';
+import App from './components/App';
 
 import './index.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
-      <AuthProvider>
-        <App />
-      </AuthProvider>
-    </BrowserRouter>
+      <App />
   </StrictMode>,
 )
